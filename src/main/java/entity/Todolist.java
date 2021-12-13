eecdedf66b2643f5b47cb75c4cc124d63cef644a
hanifiamdev <hanifamrullahdev@gmail.com>
2021-12-13 16:25:39 +0700

    IInisialisasi Maven for aplikasi java todolist

---
+++ src/main/java/entity/Todolist.java
@@ -0,0 +1,21 @@
+package entity;
+
+public class Todolist {
+
+    private String toodo;
+
+    public Todolist() {
+    }
+
+    public Todolist(String toodo) {
+        this.toodo = toodo;
+    }
+
+    public String getToodo() {
+        return toodo;
+    }
+
+    public void setToodo(String toodo) {
+        this.toodo = toodo;
+    }
+}
